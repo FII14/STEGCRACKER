179e33b20cde65422c850938f108b7d65167fdc3
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-02 20:19:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
 sudo apt-get install python3-pip -y
-pip3 install -r requirement.txt
+pip3 install -r requirements.txt
 sudo chmod +x stegcracker
 sudo mv stegcracker /usr/bin
