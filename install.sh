fa3dbc03c0bb6c54ffa43ac2ffcaaa7a890c8f15
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-02 20:27:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 sudo apt-get update -y
 sudo apt-get install steghide -y
 sudo apt-get install python3-pip -y
+sudo apt-get install wordlists
+gzip -d /usr/share/wordlists/rockyou.txt
 pip3 install -r requirements.txt
 sudo chmod +x stegcracker
 sudo mv stegcracker /usr/bin
