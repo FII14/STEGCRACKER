a6bfb6e388399054b37617a73661b936955f6223
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-02 20:23:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
-sudi apt-get install steghide -y
+sudo apt-get install steghide -y
 sudo apt-get install python3-pip -y
 pip3 install -r requirements.txt
 sudo chmod +x stegcracker
