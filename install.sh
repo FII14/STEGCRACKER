5f0c0e7cbf8f91ef0dce6c4f8a335e2f3770d57e
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-06 18:27:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,20 @@
 # @ Script  : Instal stegcracker
 # @ Pembuat : Rofi
 
+g="\e[1;32m"
+r="\e[0m"
+
 chmod +x src/stegcracker
 
-# Android 
+# Android (Termux)
 if [[ $(uname -o) == "Android" ]]; then
     pkg update
     pkg install steghide
     pkg install python3
     pip3 install -r requirements.txt
     mv src/stegcracker /data/data/com.termux/files/usr/bin
-    echo "[•] Installation completed."
-    echo "[•] You can run it by executing the command 'stegcracker --help'"
+    echo -e "${g}[•] ${r}Installation completed."
+    echo -e "${g}[•] ${r}You can run it by executing the command 'stegcracker --help'"
     exit 0
 
 # Linux Ubuntu dan Debian beserta keturunannya
@@ -22,7 +25,7 @@ elif [[ $(uname -o) == "GNU/Linux" ]]; then
     sudo apt-get install python3-pip
     pip3 install -r requirements.txt 
     mv src/stegcracker /usr/bin
-    echo "[•] Installation completed."
-    echo "[•] You can run it by executing the command 'stegcracker --help'"
+    echo -e "${g}[•] ${r}Installation completed."
+    echo -e "${g}[•] ${r}You can run it by executing the command 'stegcracker --help'"
     exit 0
 fi
